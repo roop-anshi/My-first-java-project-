fbfd7c92b3c6d9f9da9d3e0035556e8ab147c847
roop-anshi <darasingh8019@gmail.com>
2026-04-19 15:13:02 +0530

    Calculator.java
    Basic calculator using Java
    Implemented arithmetic operations like addition, subtraction, multiplication, and division
    Focused on logic building and clean code structure

---
+++ Calculator.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       System.out.println("Simple Calculator");
+        // Input numbers
+        System.out.println("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter second number: ");
+        double num2 = sc.nextDouble();
+         // Choose operation
+        System.out.println("Choose operation:");
+        System.out.println("+  -  *  /");
+        char operator = sc.next().charAt(0);
+         double result = 0;
+          switch(operator) 
+            {
+            case '+':
+               {
+                result = num1 + num2;
+                System.out.println("Result = " + result);
+                 }
+                break;
+
+            case '-':
+                {
+                result = num1 - num2;
+                System.out.println("Result = " + result);
+                 }
+                break;
+
+            case '*':
+                {
+                result = num1 * num2;
+                System.out.println("Result = " + result);
+                 }
+                break;
+
+            case '/':
+                {
+                if(num2 != 0) 
+                {
+                    result = num1 / num2;
+                    System.out.println("Result = " + result);
+                } 
+                 else 
+                {
+                    System.out.println("Error! Division by zero");
+                }
+                break;
+                }
+
+            default:
+                System.out.println("Invalid operator!");
+        } 
+}
+}
